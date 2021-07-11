c9b3ce9b2de427f509caa4edfb74d60d03e44214
Oscarwu <33107871+oscarobwu@users.noreply.github.com>
2021-07-11 20:58:49 +0800

    Update f5_login_run_v7.php

--- php_ssh/f5_login_run_v7.php
+++ php_ssh/f5_login_run_v7.php
@@ -232,3 +232,20 @@ if ( $funtion == "virtual" ) {
 }
 
 ?>
+
+##############
+$post = [
+   'teste' => $_POST['teste']
+];
+httpPost('url.com', $post);
+// function
+function httpPost($url, $data)
+{
+   	$curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($curl);
+    curl_close($curl);
+    return $response;
+}
