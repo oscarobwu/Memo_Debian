132a29fe651bc1a42bc01acfa63c2f87bbb1257a
Oscarwu <33107871+oscarobwu@users.noreply.github.com>
2021-07-17 15:06:54 +0800

    Create htpasswd-ssl_建立.sh

---
+++ htpasswd-ssl_建立.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Writes an APR1-format password hash to the provided <htpasswd-file> for a provided <username>
+# This is useful where an alternative web server (e.g. nginx) supports APR1 but no `htpasswd` is installed.
+# The APR1 format provides signifcantly stronger password validation, and is described here: 
+#	 http://httpd.apache.org/docs/current/misc/password_encryptions.html
+# 設定 sudo chmod +x htpasswd-ssl.sh
+#
+# 使用 command 方式 : sudo ./htpasswd-ssl.sh /etc/nginx/pwdfile USERNAME
+#
+help (){
+cat <<EOF
+  Usage: $0 <htpasswd-file> <username>
+  Prompts for password (twice) via openssl.
+EOF
+}
+
+[ $# -lt 2 ] && help;
+[ $# -eq 2 ] && printf "${2}:`openssl passwd -apr1`\n" >> ${1}
